6c21008ac9e8debd1c44080677fe4de1754fdfdc
Jv131103 <jjv1792@gmail.com>
2025-11-28 15:18:07 -0300

    update/atualizacao 28/11/2025

---
+++ pratica/variavel.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(){
+    int idade;  // Declaração da variável
+
+    printf("Qual a sua idade: ");
+    scanf("%d", &idade);
+
+    printf("A sua idade é: %d\n", idade);
+
+    return 0;
+}
